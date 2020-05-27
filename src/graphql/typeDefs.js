2027f564c575dc563780ae923d2434afb72d55ed
Felipe Silva <felipe.salaberga@gmail.com>
2020-05-27 20:45:51 -0300

    GraphQL with MongoDB

---
+++ src/graphql/typeDefs.js
@@ -0,0 +1,7 @@
+import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
+import path from 'path';
+
+const types = fileLoader(path.join(__dirname, 'modules', '**', '*.gql'));
+const typeDefs = mergeTypes(types);
+
+export default typeDefs;
