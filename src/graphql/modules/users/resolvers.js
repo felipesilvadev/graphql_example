44f48d0d496a67c80604ea8bc9b6a46aba247a2c
Felipe Silva <felipe.salaberga@gmail.com>
2020-06-04 14:32:42 -0300

    Added subscriptions

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,5 +1,7 @@
 import User from '../../../models/User';
 
+import { USER_ADDED } from './channels';
+
 export default {
   User: {
     fullName: (user) => `${user.firstName} ${user.lastName}`,
@@ -9,8 +11,21 @@ export default {
     user: (_, { id }) => User.findById(id),
   },
   Mutation: {
-    createUser: (_, { data }) => User.create(data),
+    createUser: async (_, { data }, { pubsub }) => {
+      const user = await User.create(data);
+
+      pubsub.publish(USER_ADDED, {
+        userAdded: user,
+      });
+
+      return user;
+    },
     updateUser: (_, { id, data }) => User.findOneAndUpdate(id, data, { new: true }),
     deleteUser: async (_, { id }) => !!(await User.findOneAndRemove(id)),
+  },
+  Subscription: {
+    userAdded: {
+      subscribe: (obj, args, { pubsub }) => pubsub.asyncIterator(USER_ADDED),
+    }
   }
 }
